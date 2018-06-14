88afab02827e90cae30589cd74ca19478f9459e7
Paul Chavard <git@paul.chavard.net>
2018-06-14 01:01:23 -0400

    Download English content

--- scripts/download.js
+++ scripts/download.js
@@ -6,30 +6,37 @@ const { formatForApp } = require('./format');
 const fs = require('fs');
 const path = require('path');
 
-base('Content Fr')
-  .select({
-    view: 'table',
-    filterByFormula: '{Published} = TRUE()'
-  })
-  .eachPage(
-    function page(records) {
-      let content = records.map(({ fields }) => fields).map(formatForApp);
+function downloadContent(locale) {
+  base(`Content ${locale}`)
+    .select({
+      view: 'table',
+      filterByFormula: '{Published} = TRUE()'
+    })
+    .eachPage(
+      function page(records) {
+        let content = records.map(({ fields }) => fields).map(formatForApp);
 
-      console.log('Downloaded ' + records.length + ' records in "Fr" locale.');
+        console.log(
+          `Downloaded ${records.length} records in "${locale}" locale.`
+        );
 
-      const dir = path.join(__dirname, '../public/content');
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
+        const dir = path.join(__dirname, '../public/content');
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir);
+        }
+        fs.writeFileSync(
+          path.join(dir, `${locale.toLowerCase()}.json`),
+          JSON.stringify(content, null, 2)
+        );
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
       }
-      fs.writeFileSync(
-        path.join(dir, 'fr.json'),
-        JSON.stringify(content, null, 2)
-      );
-    },
-    function done(err) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-    }
-  );
+    );
+}
+
+downloadContent('Fr');
+downloadContent('En');
