39574659c8f5ae6ed688fb3c5e640741b3b9f08d
Paul Chavard <git@paul.chavard.net>
2018-06-15 14:28:23 -0400

    Publish AirTable content md5

--- scripts/download.js
+++ scripts/download.js
@@ -5,10 +5,18 @@ const base = require('./base');
 const { formatForApp } = require('./format');
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 
 function toLocale(locale) {
   locale = locale.toLowerCase();
   return `${locale}-${locale}`;
+}
+
+function md5(str) {
+  return crypto
+    .createHash('md5')
+    .update(str)
+    .digest('hex');
 }
 
 function downloadContent(locale) {
@@ -19,7 +27,9 @@ function downloadContent(locale) {
     })
     .eachPage(
       function page(records) {
-        let content = records.map(({ fields }) => fields).map(formatForApp);
+        let content = records.map(({ fields }) => fields);
+        let hash = md5(JSON.stringify(content));
+        content = content.map(formatForApp);
 
         console.log(
           `Downloaded ${records.length} records in "${locale}" locale.`
@@ -29,6 +39,7 @@ function downloadContent(locale) {
         if (!fs.existsSync(dir)) {
           fs.mkdirSync(dir);
         }
+        fs.writeFileSync(path.join(dir, `${toLocale(locale)}-md5`), hash);
         fs.writeFileSync(
           path.join(dir, `${toLocale(locale)}.json`),
           JSON.stringify(content, null, 2)
